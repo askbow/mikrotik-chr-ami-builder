8d05f7941bad026ffccfcc8656380bf8e174410b
Denis Borchev <askbow@users.noreply.github.com>
2023-10-23 19:59:29 +0200

    don't pipe cats

--- download.sh
+++ download.sh
@@ -4,14 +4,14 @@ IMAGE_DATA_CACHE=mikrotik.image.data.txt
 
 curl -Ss https://mikrotik.com/download | grep -E 'chr-7\.[0-9]+\.[0-9]+\.img\.zip' | sed 's|<br />|\n|g' | sed 's|><|>\n<|g' | grep -E 'chr-7\.[0-9]+\.[0-9]+\.img\.zip' | sort -u > $IMAGE_DATA_CACHE
 
-IMAGE_URI=$(cat $IMAGE_DATA_CACHE | grep download.mikrotik.com | awk -F'"' '{print $2}' | tr -d '[:blank:]')
-IMAGE_FILE_NAME=$(cat $IMAGE_DATA_CACHE | grep download.mikrotik.com | awk -F'"' '{print $2}' | awk -F'/' '{print $6}' | tr -d '[:blank:]')
-IMAGE_MD5=$(cat $IMAGE_DATA_CACHE | grep MD5 | awk -F'>' '{print $3}' | awk -F' ' '{print $2}')
-IMAGE_SHA256=$(cat $IMAGE_DATA_CACHE | grep SHA256 | awk -F'>' '{print $3}' | awk -F' ' '{print $2}')
+IMAGE_URI=$(grep download.mikrotik.com $IMAGE_DATA_CACHE | awk -F'"' '{print $2}' | tr -d '[:blank:]')
+IMAGE_FILE_NAME=$(grep download.mikrotik.com $IMAGE_DATA_CACHE | awk -F'"' '{print $2}' | awk -F'/' '{print $6}' | tr -d '[:blank:]')
+IMAGE_MD5=$(grep MD5 $IMAGE_DATA_CACHE | awk -F'>' '{print $3}' | awk -F' ' '{print $2}')
+IMAGE_SHA256=$(grep SHA256 $IMAGE_DATA_CACHE | awk -F'>' '{print $3}' | awk -F' ' '{print $2}')
 
-echo "$IMAGE_MD5 $IMAGE_FILE_NAME" > $IMAGE_FILE_NAME.md5
-echo "$IMAGE_SHA256 $IMAGE_FILE_NAME" > $IMAGE_FILE_NAME.sha256
+echo "$IMAGE_MD5 $IMAGE_FILE_NAME" > "$IMAGE_FILE_NAME".md5
+echo "$IMAGE_SHA256 $IMAGE_FILE_NAME" > "$IMAGE_FILE_NAME".sha256
 
-wget $IMAGE_URI -O $IMAGE_FILE_NAME
-sha256sum -c $IMAGE_FILE_NAME.sha256 || exit 1
-md5sum -c $IMAGE_FILE_NAME.md5 || exit 1
+wget "$IMAGE_URI" -O "$IMAGE_FILE_NAME"
+sha256sum -c "$IMAGE_FILE_NAME".sha256 || exit 1
+md5sum -c "$IMAGE_FILE_NAME".md5 || exit 1
